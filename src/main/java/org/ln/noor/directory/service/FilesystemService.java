7a04cee396ed11818d0e4065c77b7d6cfd4426cf
Luke <luke@surino.org>
2025-12-31 15:56:58 +0100

    Add documentation to directory services

--- src/main/java/org/ln/noor/directory/service/FilesystemService.java
+++ src/main/java/org/ln/noor/directory/service/FilesystemService.java
@@ -4,8 +4,18 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
+/**
+ * Performs basic filesystem operations with minimal safeguards.
+ */
 public class FilesystemService {
 
+    /**
+     * Moves a path to a new location, creating missing parent directories.
+     *
+     * @param from source path
+     * @param to   destination path
+     * @throws IOException if the move fails
+     */
     public void move(Path from, Path to) throws IOException {
         Path parent = to.getParent();
         if (parent != null && !Files.exists(parent)) {
